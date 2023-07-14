8aab30025f6a583cdf9b634ba963e5bb0e415ac5
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-07-14 14:01:36 +0200

    Updated Spinner style

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,7 +4,8 @@ export const Spinner = () => {
   return (
     <TailSpin
       color="rgb(94 234 212)"
-      wrapperClass="bg-gray-900/25 fixed w-full h-full flex items-center justify-center"
+      wrapperClass="bg-gray-900/25 fixed top-0 left-0 w-screen h-screen
+      flex items-center justify-center"
     />
   );
 };
