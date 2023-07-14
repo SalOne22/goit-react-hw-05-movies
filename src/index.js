e99227f535a62445ebfedfbd2e3d33ed0ea317d3
SalOne22 <111443297+SalOne22@users.noreply.github.com>
2023-07-14 13:33:06 +0200

    Added react-toastify

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import { App } from 'components/App';
+import 'react-toastify/dist/ReactToastify.min.css';
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ToastContainer
+      position="top-right"
+      autoClose={2500}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+      theme="dark"
+    />
     <BrowserRouter basename="goit-react-hw-05-movies">
       <App />
     </BrowserRouter>
